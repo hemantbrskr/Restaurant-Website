2d4c4f6431105ad751f828930e50d1ace1054d2c
hemant <hunnyybarasker01@gmail.com>
2025-03-06 20:30:58 +0530

    First Commit

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection';
+import About from '../components/About';
+const Home = () => {
+  return (
+    <>
+        <HeroSection/>
+       <About/>
+     </>
+  )
+}
+
+export default Home;
---
+++ Frontend/src/Pages/Success.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const Success = () => {
+  return (
+  <>
+    <h1>success page</h1>
+  </>
+  )
+}
+
+export default Success;  // Exporting the component for use in other files.  // This component will display a success message when the reservation form is submitted successfully.  // The success message will be displayed in a green color.  // The component will also include a link back to the home page.  // The link will be styled with a hover effect and will change color to blue when hovered over.  // The link will open in a new tab when clicked.  //
