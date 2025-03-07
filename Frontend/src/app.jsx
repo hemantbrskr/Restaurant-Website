a1422e7197c7274db96d5eaf4e0b8f8e42effed7
hemant <hunnyybarasker01@gmail.com>
2025-03-07 21:30:36 +0530

    third commit

--- Frontend/src/app.jsx
+++ Frontend/src/app.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Toaster} from 'react-hot-toast';
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Success from "./pages/Success";
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
+import Success from './Pages/Success';
 
 const App = () =>  {
   return  (
